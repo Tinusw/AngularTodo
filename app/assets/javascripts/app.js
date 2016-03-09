d9608c71277c68a11f6edbe51d13b25092ff0da6
Tinus <vertelmy@gmail.com>
2016-03-09 18:21:15 +0100

    added sort by criticality, ng-submit for add, ng-click for remove

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -11,8 +11,21 @@ $(document).on('ready page:load', function() {
 app.controller('ItemsCtrl', ['$scope', function($scope){
   // Here will be all code belonging to this controller
   $scope.items =[
-	  {desc: "do groceries", done: "yes", crit: 1},
-    {desc: "cut hair", done: "no", crit: 3},
-    {desc: "fight my mom", done: "no"}
+	  {desc: "do groceries", crit: 1},
+    {desc: "cut hair",  crit: 3},
+    {desc: "fight my mom", crit: 2}
     ];
+  
+  // Add an item
+  $scope.addItem = function(){
+  	if(!$scope.newItem.desc === ''){ return; }
+  	// if user doesn't select crit value, auto-assign '3'
+  	// if(!$scope.newItem.crit === ''){ return '3'}
+	  $scope.items.push($scope.newItem);
+}
+  
+  //Delete an item
+  $scope.deleteItem = function(item){
+  	$scope.items.splice($scope.items.indexOf(item), 1);
+  }
 }]);
