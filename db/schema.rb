dff742e726770ef33d103fe9b84799adf7033b95
Tinus <vertelmy@gmail.com>
2016-03-09 20:42:02 +0100

    trying to fix post

--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,9 @@ ActiveRecord::Schema.define(version: 20160309175155) do
 
   create_table "items", force: :cascade do |t|
     t.string   "title"
+    t.integer  "crit"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "crit"
   end
 
 end
